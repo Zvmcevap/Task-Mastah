7851cf2e4065035bff150ed75c64de755675a2be
Zvmcevap <zupanc.beno@gmail.com>
2020-07-26 20:13:07 +0200

    Pain is real

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 from datetime import datetime
 
 
-#Set app and database variable
+# Set app and database variable
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
-
+bootstrap = Bootstrap(app)
 
 # SQL Alchemy database model, needs a review
 # Class of an Task featuring its data
@@ -22,6 +23,7 @@ class Todo(db.Model):
 # Application innards and stuff :S
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    current_user = "Anonymous"
     # When submitting a new task this happens
     if request.method == 'POST':
         task_content = request.form['content']
@@ -37,7 +39,23 @@ def index():
     else:
         # Tasks will return all the tasks from the db in the order they were created
         tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template("index.html", tasks=tasks)
+        return render_template("index.html", tasks=tasks, current_user=current_user)
+
+
+# PLACEHOLDERS for callendar, log-in/out!!!!!!!!!
+@app.route('/calendar/')
+def calendar():
+    return render_template('create_task.html')
+
+
+@app.route('/')
+def login():
+    return redirect('/')
+
+
+@app.route('/')
+def logout():
+    return redirect('/')
 
 
 # Delete the particular task
@@ -55,8 +73,22 @@ def delete(id):
 
 
 # Update the particular task
-@app.route('/update/<int:id>')
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
 
+        try:
+
+            db.session.commit()
+            return redirect('/')
+
+        except:
+            return "Problem updating that task"
+
+    else:
+        return render_template('update.html', task=task)
 
 
 if __name__ == "__main__":
